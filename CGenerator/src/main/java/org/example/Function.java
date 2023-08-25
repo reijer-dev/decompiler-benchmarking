e7fe9ed522bc68eb19cc611edcd3b77e651cf367
jbcjsc1 <git@bdwf.nl>
2023-08-25 22:02:29 +0200

    Comment added to IFeature and CGenerator. Refactored getNewStatement

--- CGenerator/src/main/java/org/example/Function.java
+++ CGenerator/src/main/java/org/example/Function.java
@@ -6,35 +6,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Function {
-    private String name;
-    private DataType type;
-    private final List<FunctionParameter> parameters = new ArrayList<>();
-    private final List<String> statements = new ArrayList<>();
+    private String name;                    // function name
+    private DataType type;                  // function return-datatype
+    private final List<FunctionParameter> parameters = new ArrayList<>();   // function parameter list
+    private final List<String> statements = new ArrayList<>();  // function statements
 
+    /**
+     * Construct a function object
+     * @param type  function name
+     * @param name  function return data type (may be void, may not be empty)
+     */
     public Function(DataType type, String name){
-        this.type = type;
-        this.name = name;
+        setType(type);
+        setName(name);
     }
 
+    /**
+     * Emit the code to a StringBuilder-object that accumulates all the generated c-code parts
+     * @param sb    StringBuilder to which this functions written code will be added.
+     */
     public void appendCode(StringBuilder sb){
-        sb.append('\n');
-        sb.append(type.getNameForUse());
+        sb.append('\n');                                    // new line
+        sb.append(type.getNameForUse());                    // return type
         sb.append(' ');
-        sb.append(name);
-        sb.append('(');
+        sb.append(name);                                    // function name
+        sb.append('(');                                     // list parameters
         for(var i = 0; i < parameters.size(); i++){
             if(i > 0)
                 sb.append(", ");
             parameters.get(i).appendCode(sb);
         }
         sb.append("){\n");
-        for(var statement : statements){
+        for(var statement : statements){                    // list all statements
            sb.append(statement);
            sb.append('\n');
         }
-        sb.append("\n}\n");
+        sb.append("\n}\n");                                 // close function
     }
 
+    // getters & setters
+    // =================
     public String getName() {
         return name;
     }
@@ -55,6 +66,7 @@ public class Function {
         return parameters;
     }
 
+    // Add new information to function
     public void addParameter(FunctionParameter parameter){
         parameters.add(parameter);
     }
