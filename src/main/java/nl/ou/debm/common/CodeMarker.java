0e5d32247b527bb2ba20d6d5c1347b42c763b62c
jbcjsc1 <git@bdwf.nl>
2023-10-21 22:19:07 +0200

    expanded CodeMarker class including escape characters, extended corresponding test class

--- src/main/java/nl/ou/debm/common/CodeMarker.java
+++ src/main/java/nl/ou/debm/common/CodeMarker.java
@@ -2,22 +2,81 @@ package nl.ou.debm.common;
 
 import java.util.HashMap;
 
+
+/**
+ * The CodeMarker class essentially serves as a wrapper for a hashmap containing a set
+ * of name/value-combinations. The name is the key, the value is the value.
+ * The wrapper's added value lies in mapping the map to a String that can be used in the code.
+ * The toString-method from the hash-class cannot be used, as it doesn't escape characters it uses
+ * to separate fields and values. This wrapper does!
+ * The wrapper also makes sure whenever a value is queried, the return is always a valid String-object,
+ * though of course, it may be empty.
+ */
+
 public class CodeMarker {
 
+    // constants
+    private static final String STRPROPERTYSEPARATOR=",";       // separates properties
+    private static final String STRVALUESEPARATOR=":";          // separates property name from property value
+    private static final String STRESCAPECHAR="\\";             // escapes special separator chars
+    private static final String STRPROPERTYSEPARATOR_ESC= STRESCAPECHAR + "0"; // property separator escape sequence
+    private static final String STRVALUESEPARATOR_ESC= STRESCAPECHAR + "1"; // property separator escape sequence
+    private static final String STRESCAPECHAR_ESC= STRESCAPECHAR + "2"; // property separator escape sequence
+
+    // the actual map, containing all the data
+    private final HashMap<String, String> propMap = new HashMap<>();
+
+    // constructors
+
     /**
-     *  Property is a simple struct to hold both the name of a property and its value
+     * Default constructor. Simply makes sure an empty hashmap is set up.
      */
-    private final String STRPROPERTYSEPARATOR="|";
-    private final String STRVALUESEPARATOR="=";
+    public CodeMarker() {
+        // empty constructor, does nothing at all, but needed because of
+        // the shortcut constructor
+    }
 
-    private final HashMap<String, String> propMap = new HashMap<String, String>();
+    /**
+     * Construct a new class and import values directly from the string.
+     * @param strCodedProperties    see: {@link #fromString(String)}
+     */
+    public CodeMarker(String strCodedProperties){
+        // construct directly from string
+        fromString(strCodedProperties);
+    }
 
-    public void ClearProperties(){
+    /**
+     * Construct a new class and import values directly from another codemarker
+     * @param codeMarker            property source
+     */
+    public CodeMarker(final CodeMarker codeMarker){
+        // copy constructor
+        fromCodeMarker(codeMarker);
+    }
+
+    // hashmap access
+
+    /**
+     * Clear property table
+     */
+    public void clear(){
         propMap.clear();
     }
+
+    /**
+     * Set a new value for a property (add property if not present yet)
+     * @param strPropertyName   name of the property
+     * @param strPropertyValue  value of the property
+     */
     public void setProperty(String strPropertyName, String strPropertyValue){
         propMap.put(strPropertyName, strPropertyValue);
     }
+
+    /**
+     * Get value for a property
+     * @param strPropertyName   name of the property
+     * @return                  value of the property. If not set, it returns an empty string ("")
+     */
     public String strPropertyValue(String strPropertyName){
         String out = propMap.get(strPropertyName);
         if (out == null){
@@ -25,14 +84,101 @@ public class CodeMarker {
         }
         return out;
     }
+
+    /**
+     * Remove a property from the list (if it was on the list, otherwise nothing happens)
+     * @param strPropertyName   name of the property
+     */
+    public void removeProperty(String strPropertyName){
+        propMap.remove(strPropertyName);
+    }
+
+    /**
+     * Get number of properties
+     * @return  number of properties
+     */
+    public int iNProperties(){
+        return propMap.size();
+    }
+
+    /**
+     * Map the properties to one single string, make sure that the proper characters are escaped.
+     * @return      string containing property information. Use as input for {@link #fromString(String)}
+     */
     public String toString(){
         var sb = new StringBuilder();
         for (var s : propMap.entrySet()){
-            sb.append(s.getKey());
+            sb.append(strEscapeString(s.getKey()));
             sb.append(STRVALUESEPARATOR);
-            sb.append(s.getValue());
+            sb.append(strEscapeString(s.getValue()));
             sb.append(STRPROPERTYSEPARATOR);
         }
-        return sb.toString();
+        return sb.substring(0, sb.length() -2 );
+    }
+
+    /**
+     * Initialize table from a given string. Thus, a marker that was created and exported
+     * using toString (@see toString) can be converted back to a property list, making it
+     * easier to query the properties and their values.
+     * The current list of properties is thrown out.
+     * @param strCodedProperties    String containing the property information. Use the output of
+     *                              {@link #toString()}
+     */
+    public void fromString(String strCodedProperties){
+        fromString(strCodedProperties, true);
+    }
+
+    /**
+     * Initialize table from a given string. Thus, a marker that was created and exported
+     * using toString (@see toString) can be converted back to a property list, making it
+     * easier to query the properties and their values.
+     * The current list of properties is only thrown out if flag to clear table is set. If
+     * the flag is not set, the imported information is simply applied to the existing data,
+     * adding or updating properties.
+     * @param strCodedProperties    see {@link #fromString(String)}
+     * @param bClearTable           true means table is cleared before processing
+     */
+    public void fromString(String strCodedProperties, boolean bClearTable){
+        // clear map
+        if (bClearTable) {
+            clear();
+        }
+
+        // split at properties level
+        var p = strCodedProperties.split(STRPROPERTYSEPARATOR);
+        for (var prop : p){
+            // split name/value
+            var v = prop.split(STRVALUESEPARATOR);
+            // only do something if there are exactly two entries
+            if (v.length == 2){
+                propMap.put(strDeEscapeString(v[0]), strDeEscapeString(v[1]));
+            }
+        }
+    }
+
+    /**
+     * Copy all the date from another CodeMarker object
+     * @param originalCodeMarker    data source
+     */
+    public void fromCodeMarker(final CodeMarker originalCodeMarker){
+        fromString(originalCodeMarker.toString());
+    }
+
+    /**
+     * Make sure escape characters are put in place
+     * @param strIn     raw string input
+     * @return          escaped string
+     */
+    private String strEscapeString(String strIn){
+        return strIn.replace(STRESCAPECHAR, STRESCAPECHAR_ESC).replace(STRPROPERTYSEPARATOR, STRPROPERTYSEPARATOR_ESC).replace(STRVALUESEPARATOR, STRVALUESEPARATOR_ESC);
+    }
+
+    /**
+     * Make sure escape characters are replaced by their originals
+     * @param strIn     escaped string input
+     * @return          raw string output
+     */
+    private String strDeEscapeString(String strIn){
+        return strIn.replace(STRPROPERTYSEPARATOR_ESC, STRPROPERTYSEPARATOR).replace(STRVALUESEPARATOR_ESC, STRVALUESEPARATOR).replace(STRESCAPECHAR_ESC, STRESCAPECHAR);
     }
 }
--- src/main/java/nl/ou/debm/test/CodeMarkerTest.java
+++ src/main/java/nl/ou/debm/test/CodeMarkerTest.java
@@ -3,15 +3,120 @@ package nl.ou.debm.test;
 import nl.ou.debm.common.CodeMarker;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CodeMarkerTest {
     @Test
     void BasicTests(){
         var cm = new CodeMarker();
 
-        cm.setProperty("Naam", "Jaap");
-        cm.setProperty("Adres", "Laan van Nergens 311");
-        cm.setProperty("Woonplaats", "Ergenschhuizen ZH");
+        String P[] = {"Name", "Address", "ZIP",
+                        "Key 0", "Key 1", "Key 2",
+                        "Key \\0", "Key \\1", "Key \\2",
+                        "Key ,0", "Key ,1", "Key ,2",
+                        "Key :0", "Key :1", "Key :2"};
+        String V[] = {"Harry", "Privet Drive", "O.W.L.P.O.S.T.",
+                        "A", "B", "C",
+                        "D", "E", "F",
+                        "G", "H", "I",
+                        "J", "K", "L"};
+        assertEquals(P.length, V.length);
 
-        System.out.println(cm);
+        // assert empty output on empty input
+        assertEquals("", cm.toString());
+
+        // set values and assert table size
+        int x;
+        for (x=0; x<P.length ; ++x) {
+            assertEquals(x, cm.iNProperties());
+            cm.setProperty(P[x], V[x]);
+            assertEquals(x + 1, cm.iNProperties());
+        }
+
+        // assert values
+        for (x=0; x<P.length ; ++x) {
+            assertEquals(V[x], cm.strPropertyValue(P[x]));
+        }
+
+        // shift values
+        P = shift(P,1);
+
+        // update values and assert table size
+        for (x=0; x<P.length ; ++x) {
+            cm.setProperty(P[x], V[x]);
+            assertEquals(P.length, cm.iNProperties());
+        }
+        // assert values
+        for (x=0; x<P.length ; ++x) {
+            assertEquals(V[x], cm.strPropertyValue(P[x]));
+        }
+
+
+        // shift values
+        P = shift(P,-1);
+
+        // update values and assert table size
+        for (x=0; x<P.length ; ++x) {
+            cm.setProperty(P[x], V[x]);
+            assertEquals(P.length, cm.iNProperties());
+        }
+        // assert values
+        for (x=0; x<P.length ; ++x) {
+            assertEquals(V[x], cm.strPropertyValue(P[x]));
+        }
+
+
+        // assert valid return on non-existing property
+        assertEquals("", cm.strPropertyValue("doesNotExist"));
+
+        // show result
+        System.out.println("Resulting marker o: " + cm);
+
+        // copy 1
+        var cm2 = new CodeMarker(cm.toString());
+        // assert values
+        for (x=0; x<P.length ; ++x) {
+            assertEquals(V[x], cm2.strPropertyValue(P[x]));
+        }
+
+
+
+        // show result
+        System.out.println("Resulting marker 2: " + cm2);
+
+        // copy 2
+        var cm3 = new CodeMarker(cm);
+        // assert values
+        for (x=0; x<P.length ; ++x) {
+            assertEquals(V[x], cm3.strPropertyValue(P[x]));
+        }
+
+        // clear original one
+        cm.clear();
+        // assert empty output on empty input
+        assertEquals("", cm.toString());
+        // assert empty copy works
+        cm2.fromCodeMarker(cm);
+        assertEquals("", cm2.toString());
+        // another copy
+        cm2.fromCodeMarker(cm3);
+        assertEquals(cm3.iNProperties(), cm2.iNProperties());
+
+        // remove a property
+        cm2.removeProperty(P[0]);
+        // assert count
+        assertEquals(P.length - 1, cm2.iNProperties());
+
+        // show result
+        System.out.println("Resulting marker 3: " + cm3);
+    }
+
+    private String[] shift(String[] in, int sh){
+        if (sh<0){sh+=in.length;}
+        String [] out = new String[in.length];
+        for (int x=0; x<in.length; ++x){
+            out[(x + sh) % in.length] = in [x];
+        }
+        return out;
     }
 }
