897dde108842782a7ed6458ebf2143f21fca3e39
jbcjsc1 <git@bdwf.nl>
2024-03-13 20:05:48 +0100

    TestResult tests [re]written

--- src/main/java/nl/ou/debm/common/feature1/CountNoLimitTestResult.java
+++ src/main/java/nl/ou/debm/common/feature1/CountNoLimitTestResult.java
@@ -1,8 +1,16 @@
 package nl.ou.debm.common.feature1;
 
 import nl.ou.debm.assessor.IAssessor;
+import nl.ou.debm.test.CountNoLimitTest;
 
 public class CountNoLimitTestResult extends IAssessor.CountTestResult {
+
+    public CountNoLimitTestResult(){
+        super();
+    }
+    public CountNoLimitTestResult(CountNoLimitTestResult rhs){
+        super(rhs);
+    }
     @Override
     public void aggregateValues(IAssessor.TestResult rhs) {
         assert rhs instanceof CountNoLimitTestResult : "aggregating different types of test results (CountNoLimitTestResult)";
@@ -26,5 +34,15 @@ public class CountNoLimitTestResult extends IAssessor.CountTestResult {
     @Override
     public void setHighBound(long lngHighBound) {
         // do nothing
+    }
+
+    @Override
+    public IAssessor.TestResult getNewInstance() {
+        return new CountNoLimitTestResult();
+    }
+
+    @Override
+    public IAssessor.TestResult makeCopy() {
+        return new CountNoLimitTestResult(this);
     }
 }
