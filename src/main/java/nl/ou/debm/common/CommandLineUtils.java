7e32d7ed5ad43dd080d223610e9fecb74dbbb57d
jbcjsc1 <git@bdwf.nl>
2024-03-27 11:37:23 +0100

    clii - end of morning commit

--- src/main/java/nl/ou/debm/common/CommandLineUtils.java
+++ src/main/java/nl/ou/debm/common/CommandLineUtils.java
@@ -1,30 +1,61 @@
 package nl.ou.debm.common;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.System.exit;
 
 public class CommandLineUtils {
 
     static public class ParameterDefinition{
+        public String strParameterDescriptionHeader="";
         public String strParameterDescription="";
-        public String strPrefix="";
+        final public List<String> lPrefix= new ArrayList<>();
         public char cCardinality='1';
+
+        public ParameterDefinition() {};
+        public ParameterDefinition(String strParameterDescriptionHeader, String strParameterDescription, String strPrefix, char cCardinality){
+            this.strParameterDescriptionHeader = strParameterDescriptionHeader;
+            this.strParameterDescription = strParameterDescription;
+            this.lPrefix.add(strPrefix);
+            this.cCardinality = cCardinality;
+        }
     }
 
     static public class ParsedCommandLineParameter {
         public String strParameterDescription = "";
         public String strValue = "";
+
+        public ParsedCommandLineParameter(String strParameterDescription, String strValue){
+            this.strParameterDescription = strParameterDescription;
+            this.strValue = strValue;
+        }
+
+        @Override
+        public String toString(){
+            return strParameterDescription + ":" + strValue;
+        }
     }
+
 
     private String m_strProgramName = "";
     private String m_strCopyRight = "";
+    private final  List<ParameterDefinition> m_pmd = new ArrayList<>();
 
     public CommandLineUtils(String strProgramName, String strCopyRight){
         m_strProgramName = strProgramName;
         m_strCopyRight = strCopyRight;
     }
 
-    public List<ParsedCommandLineParameter> parseCommandLineInput(String[] args){
-        return null;
+    public CommandLineUtils(String strProgramName, String strCopyRight, List<ParameterDefinition> pmd){
+        m_strProgramName = strProgramName;
+        m_strCopyRight = strCopyRight;
+        setParameterDefinitions(pmd);
+    }
+
+    public void setParameterDefinitions(List<ParameterDefinition> pmd){
+        m_pmd.clear();;
+        m_pmd.addAll(pmd);
     }
 
     public void printProgramHeader(){
@@ -36,4 +67,82 @@ public class CommandLineUtils {
         System.out.println(sb);
         System.out.println(m_strCopyRight);
     }
+
+    public void printHelp(){
+        System.out.println("HELP TEXT");
+    }
+
+    private void printError(String strError) {
+        printError(strError, 1);
+    }
+    private void printError(String strError, int iErrorNumber){
+        printProgramHeader();
+        System.out.println();
+        System.out.println("*** Error: " + strError);
+        System.out.println();
+        printHelp();
+        exit(iErrorNumber);
+    }
+
+    public List<ParsedCommandLineParameter> parseCommandLineInput(String[] args){
+        assert !m_pmd.isEmpty() : "No parameter definitions initialized";
+
+        // output
+        List<ParsedCommandLineParameter> out = new ArrayList<>(args.length);
+
+        // any parameters required?
+        boolean bAnyRequired = false;
+        for (var pd : m_pmd){
+            if ((pd.cCardinality=='1') || (pd.cCardinality=='+')){
+                bAnyRequired = true;
+                break;
+            }
+        }
+        if (args.length==0){
+            if (bAnyRequired) {
+                printError("no parameters given.");
+            }
+            printProgramHeader();
+            return out;
+        }
+
+        // array to list
+        List<String> pars = new ArrayList<>(List.of(args));
+
+        // find help argument
+        String[] HELP = { "-h", "-help", "/h", "/help", "-?", "/?"};
+        for (var a : pars){
+            for (var h : HELP){
+                if (a.trim().compareToIgnoreCase(h)==0){
+                    printProgramHeader();
+                    printHelp();
+                    exit(0);
+                }
+            }
+        }
+
+        // do all other parsing
+        while (!pars.isEmpty()){
+            boolean bFound = false;
+            for (var pm : m_pmd) {
+                for (var pf : pm.lPrefix) {
+                    if (pars.get(0).startsWith(pf)) {
+                        // found parameter!
+                        bFound = true;
+                        // copy to parsed list
+                        out.add(new ParsedCommandLineParameter(pm.lPrefix.get(0), pars.get(0).substring(pf.length())));
+                    }
+                }
+            }
+            if (!bFound){
+                printError("Parameter starts with unknown option code: " + pars.get(0),1);
+            }
+            pars.remove(0);
+        }
+
+
+        return out;
+    }
+
+
 }
--- src/main/java/nl/ou/debm/test/CommandLineUtilsTest.java
+++ src/main/java/nl/ou/debm/test/CommandLineUtilsTest.java
@@ -3,13 +3,27 @@ package nl.ou.debm.test;
 import nl.ou.debm.common.CommandLineUtils;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandLineUtilsTest {
 
     @Test
     void BasicTest(){
         String[] args = {"-o=hallo", "-i=bye"};
 
-        var me = new CommandLineUtils("Mijn programmanaam", "mijn copyright");
-        me.printProgramHeader();
+        List<CommandLineUtils.ParameterDefinition> pmd = new ArrayList<>();
+        pmd.add(new CommandLineUtils.ParameterDefinition("output",
+                "This is a long description of the output. We try to make it such at there will be line breaks in the actual output. Would we succeed?",
+                "-o=", '1'));
+        pmd.add(new CommandLineUtils.ParameterDefinition("input", "some other description", "-i=", '*' ));
+
+        var me = new CommandLineUtils("my program name", "(c) 2024 by us all", pmd);
+
+        var q = me.parseCommandLineInput(args);
+
+        for (var item : q){
+            System.out.println(item.toString());
+        }
     }
 }
