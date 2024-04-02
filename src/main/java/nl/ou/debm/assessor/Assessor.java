d589bc986b8a1eaa0f137de55f6fb8395180cfba
Reijer Klaasse <76157894+reijer-dev@users.noreply.github.com>
2024-04-02 17:41:24 +0200

    Modified progress bar characters

--- src/main/java/nl/ou/debm/assessor/Assessor.java
+++ src/main/java/nl/ou/debm/assessor/Assessor.java
@@ -117,10 +117,10 @@ public class Assessor {
             if (i1>m_iWidth) {i1=m_iWidth;}
             for (int c=0; c<m_iWidth; c++){
                 if (c<i1){
-                    m_sbBar.setCharAt(c, '█');
+                    m_sbBar.setCharAt(c, '|');
                 }
                 else {
-                    m_sbBar.setCharAt(c, '░');
+                    m_sbBar.setCharAt(c, '.');
                 }
             }
             return m_sbBar.toString();
