fe77ce1b26750607694f24feb77669b89da5dbf4
reijer-dev <76157894+reijer-dev@users.noreply.github.com>
2024-04-26 09:35:10 +0200

    Fix finding functions from hexrays

--- src/main/java/nl/ou/debm/common/feature3/Feature3CVisitor.java
+++ src/main/java/nl/ou/debm/common/feature3/Feature3CVisitor.java
@@ -63,9 +63,6 @@ public class Feature3CVisitor extends CBaseVisitor<Object> {
                 .map(CParser.DeclaratorContext::directDeclarator)
                 .map(CParser.DirectDeclaratorContext::parameterTypeList);
 
-        if(parameterTypeList.isEmpty())
-            return null;
-
         functions.put(functionId, result);
         functionsByName.put(result.getName(), result);
 
@@ -109,7 +106,7 @@ public class Feature3CVisitor extends CBaseVisitor<Object> {
 
             for(var i = 0; i < statements.size(); i++){
                 var statement = statements.get(i);
-                var statementText = textPerStatement.get(statement);
+                var statementText = textPerStatement.get(statement).replaceAll("\"\"", "");
                 if(statementText.startsWith("return"))
                     result.registerReturnStatement();
                 var functionCallsMatch = functionCallPattern.matcher(statementText);
