6e14bd32552cd7eff0a5b0990afd2e301861da70
Reijer Klaasse <76157894+reijer-dev@users.noreply.github.com>
2024-04-26 16:15:37 +0200

    Update Feature3CVisitor.java

--- src/main/java/nl/ou/debm/common/feature3/Feature3CVisitor.java
+++ src/main/java/nl/ou/debm/common/feature3/Feature3CVisitor.java
@@ -109,7 +109,7 @@ public class Feature3CVisitor extends CBaseVisitor<Object> {
 
             for(var i = 0; i < statements.size(); i++){
                 var statement = statements.get(i);
-                var statementText = textPerStatement.get(statement).replaceAll("\"\"", "");
+                var statementText = textPerStatement.get(statement).replaceAll("([^\\\\])\"\"", "$1");
                 if(statementText.startsWith("return"))
                     result.registerReturnStatement();
                 var functionCallsMatch = functionCallPattern.matcher(statementText);
