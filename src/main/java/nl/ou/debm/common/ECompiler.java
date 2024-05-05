ca5aeb7e264df1a71ba270923f0151494b3aea57
jbcjsc1 <git@bdwf.nl>
2024-05-05 23:33:01 +0200

    removed the fake second compiler

--- src/main/java/nl/ou/debm/common/ECompiler.java
+++ src/main/java/nl/ou/debm/common/ECompiler.java
@@ -2,15 +2,13 @@ package nl.ou.debm.common;
 
 import nl.ou.debm.producer.ExeBuildUsingClang;
 import nl.ou.debm.producer.IBuildExecutable;
-import nl.ou.debm.producer.JBCJoke;
 
 public enum ECompiler {
-    CLANG,JBCJOKE;
+    CLANG;
 
     public String strFileCode() {
         switch (this) {
             case CLANG ->               { return "cln"; }
-            case JBCJOKE ->             { return "jbc"; }
             default -> throw new IllegalStateException("Unexpected value: " + this);
         }
     }
@@ -18,7 +16,6 @@ public enum ECompiler {
     public String strTableCode() {
         switch (this){
             case CLANG -> { return "clang"; }
-            case JBCJOKE -> { return "JBC's joke"; }
             default -> throw new IllegalStateException("Unexpected value: " + this);
         }
     }
@@ -26,7 +23,6 @@ public enum ECompiler {
     public IBuildExecutable exeBuilder(){
         switch (this){
             case CLANG -> { return new ExeBuildUsingClang(); }
-            case JBCJOKE -> {return new JBCJoke(); }
             default -> throw new IllegalStateException("Unexpected value: " + this);
         }
     }
