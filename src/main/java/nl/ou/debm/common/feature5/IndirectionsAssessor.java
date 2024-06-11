a1b5d81eb664d75fffcbe1e1f7d37fccaa0777cb
jbcjsc1 <git@bdwf.nl>
2024-06-11 14:45:40 +0200

    wrote switch quality score description

--- src/main/java/nl/ou/debm/common/feature5/IndirectionsAssessor.java
+++ src/main/java/nl/ou/debm/common/feature5/IndirectionsAssessor.java
@@ -5,6 +5,114 @@ import nl.ou.debm.assessor.IAssessor;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+
+    Indirections assessor
+
+    What do we measure?
+    ===================
+
+    a. Switch quality score
+    b. Indirections score
+
+
+    ad a. Switch quality score
+    --------------------------
+
+    We only score switches that we can recognize as switches produced by the indirections producer
+
+    We present three statistics: average score for all switches,
+                                 average score for switches implemented using indirection
+                                 average score for switches implemented otherwise
+
+
+    We score each switch 0...10, according to this scheme:
+       I.      switch present in binary             0,     1
+       II.     correct number of cases              0, .5, 1
+       III.    correctness of case ID's             0, .5, 1
+       IV.     correctness of default branch        0,     1
+       V.      correctness of case start point      0 ...  3
+       VI.     absence of case duplication          0      1
+       VII.    absence of goto's                    0,  1, 2 +
+                                                    --------
+                                                    0 ... 10
+
+    I.   switch present in binary
+         For every switch in the LLVM, we check if it is in the decompiler output. We score when at least one
+         of the switch code markers (before/after/case start/case end) is found.
+         If these are not found, the score is set to 0, regardless of any other scores
+
+    II.  correct number of cases
+         We compare the number of cases in the decompiler output to the number of cases we know from the LLVM.
+         When equal, the switch scores 1. If not equal, we evaluate the gap.
+         When more cases are found than present in the LLVM, we score 0.
+         When fewer cases are found than present in the LLVM, we score:
+             - 0.5 if the difference is 1 case;
+             - 0.5 if the difference is 10% or less;
+             - 0   in any other case.
+         The default branch is ignored in this comparison.
+
+    III. correctness of case ID's
+         We compare all the case ID's from the LLVM data to the decompiler output.
+         We make compare two lists:
+         LLVM    decompiler output
+         0       0
+         1       1
+         2
+                 3
+         4       4
+         5       5
+         6
+         This would yield a rough score of 4/7 --> 4 pairs correct, 3 pairs incorrect.
+         Rough score 1 yields final score 1.0.
+         Rough score .7 and above yields final score 0.5
+         Otherwise, 0 final score is 0.0.
+         The default branch is ignored in the comparison.
+
+    IV.  correctness of default branch
+         We compare the presence of a default branch in the LLVM to its presence in the decompiler output.
+         If present in both or not present in both, we score 1.
+         If present in the one and not in the other, we score 0.
+
+    V:   correctness of case start point
+         We assess all the branches (default branch included) in the LLVM. For every case present in the LLVM,
+         we determine whether it is present in the decompiler output. If so, we check whether the branch
+         starts with the case start code marker. If so we, we check the caseID in the code marker to the
+         case ID in the decompiler output. When equal, we increase a counter.
+         In short; the counter is only increased if a case meets all three criteria above.
+         Final score is (3 * counter) / number_of_branches_in_LLVM
+
+    VI:  absence of case duplication
+         Case code may not be duplicated in the decompiler output. If multiple branches use the same code,
+         that should be emitted only once, but with multiple "case ...:"-references before the case.
+         A begin-case code marker may not be emitted more than it was put in the LLVM. Whenever a begin-case
+         code marker is emitted more by the decompiler than present in the LLVM, the score is 0. Otherwise,
+         we score 1.0.
+         We use the begin-case code markers, because end code markers may be duplicated for other reasons (such
+         as code duplication while decoding conditional breaks in loops)
+
+    VII: absence of goto's
+         There is no need to use any goto in switch code.
+         We score 2 if no goto's are found within the switch statement.
+         We score 1 if 1 goto is found within the switch statement.
+         More goto's means scoring 0.
+         We allow for goto's contained within compound statements within the switch statement. These compounds
+         are considered not to be part of the switch code itself, but of the code within cases, for example a
+         nested loop that may use goto to break out of multiple loops.
+
+
+    ad b. Indirections score
+    ------------------------
+
+    
+
+
+
+
+
+ */
+
+
 public class IndirectionsAssessor implements IAssessor {
     @Override
     public List<TestResult> GetTestResultsForSingleBinary(CodeInfo ci) {
